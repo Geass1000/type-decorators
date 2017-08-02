b9e9e997de6239eeedf047075fa0befe2abf4a6c
Geass <geass1000@gmail.com>
2017-08-02 16:20:06 +0300

    Add: New description and interfaces

--- src/is-string.decorator.ts
+++ src/is-string.decorator.ts
@@ -1,22 +1,21 @@
-import { fnProperty } from './validator.decorator';
+import { fnProperty, PropertyDecorator } from './validator.decorator';
 
 const mkIsString : symbol = Symbol('isString');
 
 /**
  * isString - does registration all property decorators
  *
- * @param  {Object} target - Reference to the object (Class)
- * @param  {string|symbol} propertyKey - Property name (method name) in the target
- * @param  {number} parameterIndex - Index param in the method
- * @return {void}
+ * @param  {Object} target - reference to the object (Class)
+ * @param  {string|symbol} propertyKey - property name (method name) in the target
+ * @param  {number} parameterIndex - param index in the method
+ * @return {function} - function decorator
  */
-export function isString () :
- 	(target : Object, propertyKey : string | symbol, parameterIndex : number) => void {
+export function isString () : PropertyDecorator {
 	return fnProperty(mkIsString, fnValidator);
 }
 
-function fnValidator (data : any) {
+function fnValidator (data : any, index ?: number) {
 	if (typeof data !== 'string') {
-
+		throw new TypeError(`The operand ${index} must have type string`);
 	}
 }
